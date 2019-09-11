aaf823d716c3c7d38e45a730aee598846dddb13b
flit ms <flitms@gmail.com>
2019-09-12 00:24:04 +0800

    Updates

---
+++ precache-manifest.29c25c9ae0a33a14e1ec10fc22caffc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e01cb510bf64d5c3d7d297b5733c74",
+    "url": "/flit/index.html"
+  },
+  {
+    "revision": "92192a585572ee99bd0d",
+    "url": "/flit/static/css/2.16c182ab.chunk.css"
+  },
+  {
+    "revision": "1fb56287019f3d96e62b",
+    "url": "/flit/static/css/main.2c785df7.chunk.css"
+  },
+  {
+    "revision": "92192a585572ee99bd0d",
+    "url": "/flit/static/js/2.cf8e3df2.chunk.js"
+  },
+  {
+    "revision": "1fb56287019f3d96e62b",
+    "url": "/flit/static/js/main.b1873d2d.chunk.js"
+  },
+  {
+    "revision": "79baaabf2b8e2cb7ad97",
+    "url": "/flit/static/js/runtime~main.a1e79ede.js"
+  }
+]);
